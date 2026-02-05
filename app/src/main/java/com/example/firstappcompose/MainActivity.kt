814837f9886e24cca587a5207b79fa54bd319b5a
Xavier <xavier.sotomayor21@hotmail.com>
2026-02-04 23:11:45 -0600

    fix minor imports issues

--- app/src/main/java/com/example/firstappcompose/MainActivity.kt
+++ app/src/main/java/com/example/firstappcompose/MainActivity.kt
@@ -15,7 +15,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.atom.firstappcompose.ui.theme.FirstAppComposeTheme
+import com.example.firstappcompose.ui.theme.FirstAppComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
