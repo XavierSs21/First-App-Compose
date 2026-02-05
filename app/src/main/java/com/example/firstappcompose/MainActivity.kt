795ac5ce0d3a3abe1cfc23fca2178c04d9d02466
Xavier <xavier.sotomayor21@hotmail.com>
2026-02-04 23:07:37 -0600

    initial commit with 'Tarea 1 hecha' features

---
+++ app/src/main/java/com/example/firstappcompose/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.atom.firstappcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.atom.firstappcompose.ui.theme.FirstAppComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FirstAppComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    InfoScreen(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun InfoScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Xavier Sotomayor Saldivar",
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF6200EE)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "22130833",
+            fontSize = 24.sp,
+            color = Color.Gray
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun InfoScreenPreview() {
+    FirstAppComposeTheme {
+        InfoScreen()
+    }
+}
